0ca5984c1129c4ed4ca2315c03cdfb4ee093ae47
Jacob Paul <jacobpau75608@gmail.com>
2026-03-13 14:54:25 +0530

    second

--- Tools/Components/AppVersionChecker.js
+++ Tools/Components/AppVersionChecker.js
@@ -25,8 +25,8 @@ export default function AppVersionChecker(props){
   
   const Colors=useTheme();
   i18n.translations = state.i18ntranslation;
-  CurrentiOSversion='2.3.5';
-  CurrentAndroidversion='2.3.5';
+  CurrentiOSversion='2.3.6';
+  CurrentAndroidversion='2.3.6';
   const [needUpdate,setNeedUpdate]=useState(false);
   const [canshow,setcanshow]=useState(false);
   
